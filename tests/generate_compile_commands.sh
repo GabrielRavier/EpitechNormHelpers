5ba8e9467dd3473ec78ee1fc1429c78d3bb2e92b
Gabriel Ravier <gabravier@gmail.com>
2020-05-20 10:36:50 +0200

    tests/generate_compile_commands: Correct directory for o3 files
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- tests/generate_compile_commands.sh
+++ tests/generate_compile_commands.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")" || exit
-bear -o o3/compile_commands.json cc -c o3/{success,failure}.c
+bear -o data/o3/compile_commands.json cc -c data/o3/{success,failure}.c
 rm {success,failure}.o
