0f4b1c4149e529961200965d823a0cbf13732605
Gabriel Ravier <gabravier@gmail.com>
2020-05-25 16:01:21 +0200

    src/main: Made initial clang-format file that works with main
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-#include <cxxopts.hpp>
-#include <fmt/format.h>
-#include <exception>
-#include <iostream>
+#include "diagnostic.hpp"
 #include "options.hpp"
 #include "program.hpp"
-#include "diagnostic.hpp"
+#include <cxxopts.hpp>
+#include <exception>
+#include <fmt/format.h>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
@@ -14,11 +14,11 @@ int main(int argc, char *argv[])
 		program(options_parser::parse_options(argc, argv));
 		return EXIT_SUCCESS;
 	}
-	catch (const cxxopts::OptionParseException& option_parse_exception)
+	catch (const cxxopts::OptionParseException &option_parse_exception)
 	{
 		diagnostic::fatal_error(fmt::format("failed to parse options: {}", option_parse_exception.what()));
 	}
-	catch (const std::exception& exception)
+	catch (const std::exception &exception)
 	{
 		diagnostic::fatal_error(fmt::format("exception thrown: {}", exception.what()));
 	}
