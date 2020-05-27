1519813406d4ae834e9251a2c6772ef865c80d55
Gabriel Ravier <gabravier@gmail.com>
2020-05-27 13:27:20 +0200

    epitech-norm-tool/src/checks/o4.cpp: Minimized includes
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- epitech-norm-tool/src/checks/o4.cpp
+++ epitech-norm-tool/src/checks/o4.cpp
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include "checks/o4.hpp"
-#include "basename.hpp"
-#include "checks/checks.hpp"
-#include "diagnostic.hpp"
-#include "managers.hpp"
-#include "regex-utils.hpp"
-#include <boost/regex.hpp>
-#include <fmt/format.h>
+#include <boost/regex.hpp>	// for boost::regex
+#include <string_view>	// for std::string_view
+#include "basename.hpp"	// for basename_wrappers::base_name
+#include "checks/checks.hpp"	// for checks::level_t
+#include "managers.hpp"	// for managers::resources_manager
+#include "regex-utils.hpp"	// for regex_utils::warn_match_in_check, regex_utils::simple_regex_search, etc.
 
 static void warn_match(std::string_view matched_string, checks::level_t level)
 {
