787bd8fd3905352da9ebfe5ff6a46693caf8d72d
Gabriel Ravier <gabravier@gmail.com>
2020-07-30 19:24:10 +0200

    epitech-norm-tool/src/checks/f4.cpp: Just made it a stub since this looks pretty hard to do properly
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- epitech-norm-tool/src/checks/f4.cpp
+++ epitech-norm-tool/src/checks/f4.cpp
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include "checks/f4.hpp"
 #include "checks/checks.hpp"		  // for checks::level_t
+#include "checks/stubs.hpp"			  // for checks::stubs::unimplementable
 #include "diagnostic.hpp"			  // for diagnostic::warn
 #include "managers.hpp"				  // for managers::resource_manager
 #include <cppast/cpp_entity.hpp>	  // for cppast::cpp_entity
@@ -10,21 +11,25 @@
 #include <cstddef>					  // for std::size_t
 #include <fmt/core.h>				  // for fmt::format
 
-// Level 1 checks for functions containing more than 20 statements
-static void do_level1_one_file(const cppast::cpp_file &parsed_file)
+// Level 1 checks for functions containing more than 20 statements (TODO, I have absolutely no idea of how to do this with cppast and there's a pretty good chance it's just impossible)
+[[maybe_unused]] static void do_level1_one_file(const cppast::cpp_file &parsed_file)
 {
 	cppast::visit(
 		parsed_file,
 		[](const cppast::cpp_entity &entity) {
 			return cppast::is_function(entity.kind());
 		},
-		[](const cppast::cpp_entity &entity, const cppast::visitor_info &information) {
+		[]([[maybe_unused]] const cppast::cpp_entity &entity, [[maybe_unused]] const cppast::visitor_info &information) {
+
 		});
 }
 
-void checks::f4::do_check(checks::level_t, managers::resources_manager &check_resource_manager)
+void checks::f4::do_check(checks::level_t, [[maybe_unused]] managers::resources_manager &check_resource_manager)
 {
-	auto parsed_files_list = check_resource_manager.cppast.request_parsed_files_list();
-	for (const auto &parsed_file : parsed_files_list)
-		do_level1_one_file(parsed_file);
+	// Attempted implementation that doesn't work for now
+	/*
+	 * auto parsed_files_list = check_resource_manager.cppast.request_parsed_files_list();
+	 * std::for_each(parsed_files_list.begin(), parsed_files_list.end(), do_level1_one_file);
+	 */
+	checks::stubs::unimplementable("f4");
 }
