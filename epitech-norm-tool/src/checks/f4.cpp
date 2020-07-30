8ed7ef1f1a0dffa769e39f7c35a1621f1104fd02
Gabriel Ravier <gabravier@gmail.com>
2020-07-30 23:44:43 +0200

    utils-files/clang-tidy: Replaced clang-tidy.txt with .clang-tidy and added f4 to it
    Changes:
    - Changed README.md and epitech-norm-tool/src/checks/stubs.cpp to reflect the move from clang-tidy.txt to .clang-tidy
    - Changed epitech-norm-tool/src/checks/f4.cpp to reflect the addition of f4 to .clang-tidy
    - Changed epitech-norm-tool/src/checks/stubs.cpp to make the string for beginning advice to have a better name
    
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- epitech-norm-tool/src/checks/f4.cpp
+++ epitech-norm-tool/src/checks/f4.cpp
@@ -1,35 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #include "checks/f4.hpp"
-#include "checks/checks.hpp"		  // for checks::level_t
-#include "checks/stubs.hpp"			  // for checks::stubs::unimplementable
-#include "diagnostic.hpp"			  // for diagnostic::warn
-#include "managers.hpp"				  // for managers::resource_manager
-#include <cppast/cpp_entity.hpp>	  // for cppast::cpp_entity
-#include <cppast/cpp_entity_kind.hpp> // for cppast::is_function
-#include <cppast/cpp_file.hpp>		  // for cppast::cpp_file
-#include <cppast/visitor.hpp>		  // for cppast::visit, cppast::visitor_info
-#include <cstddef>					  // for std::size_t
-#include <fmt/core.h>				  // for fmt::format
+#include "checks/stubs.hpp"			  // for checks::stubs::done_in_clang_tidy
 
-// Level 1 checks for functions containing more than 20 statements (TODO, I have absolutely no idea of how to do this with cppast and there's a pretty good chance it's just impossible)
-[[maybe_unused]] static void do_level1_one_file(const cppast::cpp_file &parsed_file)
+void checks::f4::do_check(checks::level_t ,managers::resources_manager &)
 {
-	cppast::visit(
-		parsed_file,
-		[](const cppast::cpp_entity &entity) {
-			return cppast::is_function(entity.kind());
-		},
-		[]([[maybe_unused]] const cppast::cpp_entity &entity, [[maybe_unused]] const cppast::visitor_info &information) {
-
-		});
-}
-
-void checks::f4::do_check(checks::level_t, [[maybe_unused]] managers::resources_manager &check_resource_manager)
-{
-	// Attempted implementation that doesn't work for now
-	/*
-	 * auto parsed_files_list = check_resource_manager.cppast.request_parsed_files_list();
-	 * std::for_each(parsed_files_list.begin(), parsed_files_list.end(), do_level1_one_file);
-	 */
-	checks::stubs::unimplementable("f4");
+	checks::stubs::done_in_clang_tidy("f4");
 }
--- epitech-norm-tool/src/checks/stubs.cpp
+++ epitech-norm-tool/src/checks/stubs.cpp
@@ -3,9 +3,9 @@
 #include <fmt/core.h>	  // for fmt::format
 #include <string_view>	  // for std::string_view
 
-#define STR_NOT_IMPLEMENTED_PLS_LOOK_AT "this check is not implemented in the tool, you may want to"
+#define STR_NOT_IMPLEMENTED_MAY_WANT_TO_LOOK_AT "this check is not implemented in the tool, you may want to"
 #define STR_CLANG_FORMAT_USE "the provided .clang-format file"
-#define STR_CLANG_TIDY_LOOK_AT "clang-tidy.txt"
+#define STR_CLANG_TIDY_USE "the provided .clang-tidy file"
 #define STR_GNU_COMPILER_LOOK_AT "gnu-warnings.txt"
 #define STR_FOR_NOTES_HOW_DO_THIS_CHECK_WITH "for notes on how to do this check with"
 #define STR_GNU_COMPILER_WITH "your compiler (if you use a GNU-like compiler like GCC or Clang)"
@@ -13,22 +13,22 @@
 
 void checks::stubs::done_in_clang_format(std::string_view check_name)
 {
-	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_PLS_LOOK_AT " use " STR_CLANG_FORMAT_USE, check_name));
+	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_MAY_WANT_TO_LOOK_AT " use " STR_CLANG_FORMAT_USE, check_name));
 }
 
 void checks::stubs::done_in_clang_tidy(std::string_view check_name)
 {
-	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_PLS_LOOK_AT " look at " STR_CLANG_TIDY_LOOK_AT " " STR_FOR_NOTES_HOW_DO_THIS_CHECK_WITH " " STR_CLANG_TIDY_WITH, check_name));
+	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_MAY_WANT_TO_LOOK_AT " use " STR_CLANG_TIDY_USE " " STR_FOR_NOTES_HOW_DO_THIS_CHECK_WITH " " STR_CLANG_TIDY_WITH, check_name));
 }
 
 void checks::stubs::done_in_compiler(std::string_view check_name)
 {
-	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_PLS_LOOK_AT " look at " STR_GNU_COMPILER_LOOK_AT " " STR_FOR_NOTES_HOW_DO_THIS_CHECK_WITH " " STR_GNU_COMPILER_WITH, check_name));
+	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_MAY_WANT_TO_LOOK_AT " look at " STR_GNU_COMPILER_LOOK_AT " " STR_FOR_NOTES_HOW_DO_THIS_CHECK_WITH " " STR_GNU_COMPILER_WITH, check_name));
 }
 
 void checks::stubs::done_in_compiler_and_clang_tidy(std::string_view check_name)
 {
-	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_PLS_LOOK_AT " look at " STR_GNU_COMPILER_LOOK_AT " or " STR_CLANG_TIDY_LOOK_AT " " STR_FOR_NOTES_HOW_DO_THIS_CHECK_WITH " " STR_GNU_COMPILER_WITH " or " STR_CLANG_TIDY_WITH, check_name));
+	diagnostic::error(fmt::format("{}: " STR_NOT_IMPLEMENTED_MAY_WANT_TO_LOOK_AT " look at " STR_GNU_COMPILER_LOOK_AT " or use " STR_CLANG_TIDY_USE " " STR_FOR_NOTES_HOW_DO_THIS_CHECK_WITH " " STR_GNU_COMPILER_WITH " or " STR_CLANG_TIDY_WITH, check_name));
 }
 
 void checks::stubs::unimplementable(std::string_view check_name)
