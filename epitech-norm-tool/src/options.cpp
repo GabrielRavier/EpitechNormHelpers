8af2bd6465b9f9bfa6e87dba8cc2989868b30956
Gabriel Ravier <gabravier@gmail.com>
2020-09-30 18:35:59 +0200

    Corrected help message for certain options and added implicit value for --check-all
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

--- epitech-norm-tool/src/options.cpp
+++ epitech-norm-tool/src/options.cpp
@@ -26,7 +26,7 @@ static void make_options_from_check_list(cxxopts::Options &options, const checks
 			auto option_name_string = fmt::format("check-{}", check_information.short_name);
 			auto description_string = fmt::format("Enable '{}' check ", check_information.name);
 			if (check_information.maximum_level > 1)
-				description_string += fmt::format("(levels 1 through {}", check_information.maximum_level);
+				description_string += fmt::format("(levels 1 through {})", check_information.maximum_level);
 			else
 				description_string += "(level 1 only)";
 
@@ -101,7 +101,7 @@ options_parser::parsed_options options_parser::parse_options(int argc, char *arg
 	make_options_from_check_list(options, global_check_list);
 
 	options.add_options()
-			("check-all", "Enable all checks at the specified level", cxxopts::value<unsigned>())
+			("check-all", "Enable all checks at the specified level", cxxopts::value<unsigned>()->implicit_value("1"))
 			("directory", "Directory in which the tool will run. All other paths are relative to this one", cxxopts::value<std::string>()->default_value("."))
 			("compile-commands-directory", "Directory containing a compile_commands.json file", cxxopts::value<std::string>()->default_value("."))
 			("h,help", "Print usage");
